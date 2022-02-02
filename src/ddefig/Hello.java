b946eb61e298f618dff2a75154f79baea964178c
MandyChadwick <cha33dwick@gmail.com>
2022-02-02 09:31:08 -0900

    commint with Mandy name

--- src/ddefig/Hello.java
+++ src/ddefig/Hello.java
@@ -3,5 +3,6 @@ package ddefig;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Denis");
+        System.out.println("Hello, my name is Mandy")
     }
 }
