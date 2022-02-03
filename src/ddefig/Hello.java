23214cfb96291ff94440ff3385c68fd400f39cd7
meldac <97717430+meldac@users.noreply.github.com>
2022-02-02 21:01:38 -0700

    New update for Dace

--- src/ddefig/Hello.java
+++ src/ddefig/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Denis");
         System.out.println("Hello, my name is Mandy");
+        System.out.println("Hello, my name is Dace");
     }
 }
